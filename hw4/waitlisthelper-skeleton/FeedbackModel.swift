91bd41969eb7af8024c303c8aba64f14093eaa2f
Christy Quang <christyquang@berkeley.edu>
2022-03-10 23:14:18 -0800

    final submission

---
+++ hw4/waitlisthelper-skeleton/FeedbackModel.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  waitlisthelper-skeleton
+//
+//  Created by Christy Quang on 3/6/22.
+//
+
+import Foundation
+
+let feedbackModel =
+    [(90, "You're good 😄"),
+     (60, "You're probably okay ☺️"),
+     (40, "Idk, it's like 50-50 😐"),
+     (20, "You're probably not getting off 😕"),
+     (0, "Rip 😔")]
+
+var test = "test"
