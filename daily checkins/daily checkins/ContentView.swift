bdde14faf1a2ef94d219a4d3312220aadd5f5d62
Christy Quang <christyquang@berkeley.edu>
2022-05-03 10:56:28 -0700

    Log in

---
+++ daily checkins/daily checkins/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  daily checkins
+//
+//  Created by Christy Quang on 5/2/22.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+struct ContentView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var isLoggedIn: Bool = false
+    
+    var body: some View {
+        VStack {
+            Image("seed 2")
+            Text("Log In")
+                .foregroundColor(.red)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+            TextField("Email", text: $email)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            
+            TextField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+                
+            Text("Log In")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+                
+            Text("Don't have an account?")
+            Text("Sign Up")
+                .foregroundColor(Color.blue)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ daily checkins/daily checkins/daily_checkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  daily_checkinsApp.swift
+//  daily checkins
+//
+//  Created by Christy Quang on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct daily_checkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
